b3c64cf083e484a8cb48866d7a5eaa310dcc2240
Sam SO <nysamkees@gmail.com>
2024-01-30 18:34:37 +0000

    first commit

---
+++ .ipynb_checkpoints/garden_path_sentences-checkpoint.py
@@ -0,0 +1,2 @@
+inport spacy
+
---
+++ garden.py
@@ -0,0 +1,9 @@
+import spacy
+
+senOne = str("The man who hunts ducks out on weekends")
+senTwo = str("Mary gave the child the dog bit a bandaid")
+senThree = str("We painted the wall with cracks")
+gardenpathSentences = [senOne, senTwo, senThree]
+
+print (gardenpathSentences)
+gardenpathSentences.split()
