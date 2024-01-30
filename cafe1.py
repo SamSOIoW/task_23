5d413c5313e5c5fe5f2c6c18a7674baced6fedf2
Sam Simpson Oldale <153547156+SamSOIoW@users.noreply.github.com>
2024-01-30 19:26:15 +0000

    Update cafe1.py
    updated file

--- cafe1.py
+++ cafe1.py
@@ -1,16 +1,19 @@
 # creation of list of items
-menu = ["risotto", "chicken alfredo", "antipasti", "lasagne", "garlic bread", "pizza"] 
+menu = ["risotto", "chicken alfredo", "antipasti", "lasagne", "garlic bread", "pizza"]
 # creation of first dictionary to show stock levels, linked to menu list
-stock = {'risotto': 25,'chicken alfredo': 8, 'antipasti': 15, 'lasagne': 34, 'garlic bread': 18, 'pizza': 28} 
+stock = {'risotto': 25,'chicken alfredo': 8, #
+         'antipasti': 15, 'lasagne': 34, 'garlic bread': 18, 'pizza': 28} 
 # creation of second dictionary to show prices, linked to menu list
-price = {'risotto': 14.50, 'chicken alfredo': 16.50, 'antipasti': 22.00, 'lasagne': 12.28, 'garlic bread': 4.25, 'pizza': 11.00}
+price = {'risotto': 14.50, 'chicken alfredo': 16.50, 'antipasti': 22.00,
+         'lasagne': 12.28, 'garlic bread': 4.25, 'pizza': 11.00}
 
 total_value = 0
 stock_num = 0
 for i in menu: # creation of for loop
-
-    total_value = total_value + stock[i] * price[i] # calculation of total value of stock * price per item
+# calculation of total value of stock * price per item
+    total_value = total_value + stock[i] * price[i]
 print ("Total items in stock:" ,sum(stock.values())) # calculates the sum of the  items in stock
 print ()
 print ("The total value of the stock is: £" + str(total_value)) # print total_value defined above
-# adding for commit
+print()
+# End-of-file (EOF)
